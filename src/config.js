eb5f2c7607c641f8bd27604bf1bd68e57f09d2c6
chunxiao01 <chunxiao01@outlook.com>
2022-06-16 22:55:47 +0800

    update config.js

--- src/config.js
+++ src/config.js
@@ -2,9 +2,9 @@ var _config = {
   blog_name: "时光 moment", // 博客名称
   blog_url: "", //博客地址
   blog_pub_url: "", //博客发布地址
-  owner: "cxycool", // github 用户名
-  nickname: "soda", //用户昵称
-  repo: "moment", // github 中对应仓库名
+  owner: "chunxiao01", // github 用户名
+  nickname: "凤梨罐头也过期", //用户昵称
+  repo: "my-moment", // github 中对应仓库名
   blog_author_type: "OWNER", //发布博客的用户 默认 OWNER
   access_token: "ghp_M93K4IubfbVo"+"61hXCXsMwiRNMoRVII1eOyaq", // 请求量大时需要在 github 后台单独设置一个读取公开库的 token, 注意将token 拆成两个字符串，否则会被系统自动删除掉
   per_page: 5, // 默认一页显示几篇文章
