d532dcbe667883800755e6ef68df1d7518b58718
Chun Xiao <chunxiao01@outlook.com>
2022-06-19 23:24:39 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 var _config = {
-  blog_name: "时光 moment", // 博客名称
+  blog_name: "匆匆时光，百味人生", // 博客名称
   blog_url: "", //博客地址
   blog_pub_url: "", //博客发布地址
   owner: "chunxiao01", // github 用户名
-  nickname: "凤梨罐头也过期", //用户昵称
+  nickname: "阿C", //用户昵称
   repo: "my-moment", // github 中对应仓库名
   blog_author_type: "OWNER", //发布博客的用户 默认 OWNER
   show_friend: false, //是否展示好友
